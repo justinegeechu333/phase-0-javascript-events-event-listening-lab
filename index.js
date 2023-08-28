4a28b28aa9e0ab9b0eadc5c60d1a440c7f3f3b26
Justine <Justinegeechu333@gmail.com>
2023-08-27 23:44:34 -0500

    Javascripts Events

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+	const btn = document.getElementById('button');
+	btn.addEventListener('click', function () {
+		alert('I was clicked!');
+	});
 }
+addingEventListener();
